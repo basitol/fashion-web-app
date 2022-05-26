6e8af7ea59b11dceb67d74d94f9b3ab094f2a70d
basitol <41623944+basitol@users.noreply.github.com>
2022-05-26 23:52:35 +0100

    Update: App component with router, container

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Header, Footer } from "./components";
+import { Homepage, ProductPage } from "./pages";
+import { Container } from "react-bootstrap";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Container>
+        <Routes>
+          <Route index path="/" element={<Homepage />}></Route>
+          <Route path="/product/:id" element={<ProductPage />}></Route>
+        </Routes>
+      </Container>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
