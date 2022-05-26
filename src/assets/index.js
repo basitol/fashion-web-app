7118cde158b75fa3431195d655b532bc8cd442f1
basitol <41623944+basitol@users.noreply.github.com>
2022-05-27 00:01:20 +0100

    build export for assets

---
+++ src/assets/index.js
@@ -0,0 +1,11 @@
+import logo from "./logo.png";
+import airpod from "./airpods.jpg";
+import camera from "./camera.jpg";
+import mouse from "./mouse.jpg";
+import phone from "./phone.jpg";
+import game from "./playstation.jpg";
+import alexa from "./alexa.jpg";
+import comfy from "./comfy.jpg";
+import toper from "./toper.jpg";
+
+export { logo, airpod, camera, mouse, phone, game, alexa, comfy, toper };
